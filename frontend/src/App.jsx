279b72663bc28ba756be58174a6c81e9cf64d087
RonxldWilson <ronaldwilson00000@gmail.com>
2024-10-30 23:39:06 +0530

    website-skeleton-and-copy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header' style={{ display:'flex', justifyContent:'space-between'}}>
+        <div className='logo' > Logo </div>
+        <div className='right-menu' style={{display:'flex'}}>
+          <div>Services</div>
+          <div>Team</div>
+          <div>FAQ</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='section-Landing'>
+        <h1>Holistic Mental Wellness <br /> For Everyone</h1>
+        <h2>We are your go to destination for anything mental health. We offer carefully tailored mental wellness programs with expert guidance and community support that take you from surviving to thriving.</h2>
+        <button>Contact Us</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className='section-meet-team'> 
+        <h2> WE RALLY BEHIND THE NAME OF OUR ORGANIZATION.  We are here to break the stigma around mental health and provide individuals with tools and support on their healing journey.</h2>
+        
+        <div>
+          <img src="" alt="Image" />
+           Card With Detaiils
+        </div>
+        
+        <div>
+          <img src="" alt="Image" />
+           Card With Detaiils
+        </div>
+        
+        <div>
+          <img src="" alt="Image" />
+           Card With Detaiils
+        </div>
+
+      </div>
+
+      <div className='section-bay-program'>
+      <h4>At Better Together, we don’t believe in short term solutions, but sustainable ones. The Bright As You Program brings a holistic balance between the mental, emotional, and physical.</h4>
+      
+      BAY Card 01
+      BAY Card 02
+      BAY Card 03
+      BAY Card 04 
+      BAY Card 05
+      </div>
+
+      <div className='section-Testimonials'>
+
+      </div>
+
+      <div className='section-faq'>
+
+      </div>
+npm
+      <div className='section-contact-us'>
+      Have Questions? We’re an email away!
+
+      Reach us at query.bettertogether@gmail.com for questions, queries, or collaborations.
+
+      Or send us a message on our social media!
+
+      </div>
+      
+      <div className='section-footer'>
+
+      </div>
     </>
   )
 }
